298d92cb4ea64e805a8ca1067892e7127d9beeb6
Kaushik Gohain Bora <70643946+sp4chi@users.noreply.github.com>
2026-04-17 16:42:48 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int M = matrix.length;
+        int N = matrix[0].length;
+
+        int startRow = 0;
+        int endRow = M - 1;
+
+        while(startRow <= endRow){
+            int midRow = (startRow + endRow)/2;
+
+            // target found
+            if(target == matrix[midRow][0]){
+                return true;
+            }// target not found but less than first element in mid row
+            else if(target < matrix[midRow][0]){
+                endRow = midRow - 1;    
+            }// target not less than first element in mid row, 
+            //  could be in this row, search
+            else{
+                int startCol = 0;
+                int endCol = N - 1;
+                while(startCol <= endCol){
+                    int midCol = (startCol + endCol )/2;
+                    if(target == matrix[midRow][midCol]){
+                        return true;
+                    }else if(target < matrix[midRow][midCol]){
+                        endCol = midCol - 1;
+                    }else {
+                        startCol = midCol + 1;
+                    }
+                }
+
+                // loop stops, that means element not found, 
+                // change search space
+                startRow = midRow + 1;
+            }
+        }
+
+        return false;
+        
+    }
+}
